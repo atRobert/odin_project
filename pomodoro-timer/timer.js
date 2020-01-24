266d8da8f02412cab6ab3300376082078db52da6
Robert <rjbarrett04@gmail.com>
2020-01-24 03:05:24 -0800

    Pomodoro-timer 25min work 5 min break, resets.

---
+++ pomodoro-timer/timer.js
@@ -0,0 +1,77 @@
+
+let minutesDisplay = document.querySelector('#minutes')
+let secondsDisplay = document.querySelector('#seconds')
+let status = document.querySelector('#status')
+let minutes
+let seconds
+let startTime = 10
+let isPaused = true
+let currentTimer = 'work'
+
+
+setInterval(function(){
+    isPaused ? {} : runClock();  
+}, 1000)
+
+function runClock(){
+    startTime = --startTime
+    minutesDisplay.textContent = getMinutes();
+    secondsDisplay.textContent = getSeconds();
+    checkSwap()
+}
+
+function checkSwap(){
+    if (minutes == 0 && seconds == 0){
+        if (currentTimer == 'work'){
+            resetBreak()
+            play()
+        } else {
+            resetTime()
+            play()
+        }
+    }
+}
+
+function getSeconds(){
+    seconds = startTime % 60;
+    if (seconds == 0){
+        seconds = '00'
+    } else if (seconds < 10){
+        seconds = "0" + seconds 
+    }
+    console.log(seconds)
+    return seconds
+}
+
+function getMinutes(){
+    minutes = startTime / 60;
+    minutes < 1 ? minutes = '00' : minutes = Math.floor(startTime / 60)
+    return minutes
+}
+
+function resetTime(){
+    pause()
+    status.textContent = 'Working Time!'
+    currentTimer = 'work'
+    startTime = 1500
+    minutesDisplay.textContent = 25
+    secondsDisplay.textContent = '00'
+}
+
+function resetBreak(){
+    pause()
+    status.textContent = 'Break Time!'
+    currentTimer = 'break'
+    startTime = 300
+    minutesDisplay.textContent = 5
+    secondsDisplay.textContent = '00'
+}
+
+function pause(){
+    isPaused = true
+}
+
+function play(){
+    isPaused = false
+}
+
