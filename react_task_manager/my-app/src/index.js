586ddad07543d1c66e818270c42e83d3b0d50a1b
Robert <rjbarrett04@gmail.com>
2020-02-17 01:01:58 -0800

    minor update to CSS. Fixed Cross Error. Need Way to add project.

--- react_task_manager/my-app/src/index.js
+++ react_task_manager/my-app/src/index.js
@@ -1,5 +1,5 @@
 
-import * as serviceWorker from './serviceWorker';
+
 
 import React from 'react';
 import ReactDOM from 'react-dom';
@@ -76,7 +76,7 @@ ReactDOM.render(
     document.getElementById('root')
   );
 
-serviceWorker.unregister();
+
 
 
 
