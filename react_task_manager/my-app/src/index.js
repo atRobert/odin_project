d6feaaf630bafab0488f81c3f12f4fd78a2e2fa0
Robert <rjbarrett04@gmail.com>
2020-02-16 16:40:52 -0800

    Tabs on sides are selectable. Slight pixel off..

--- react_task_manager/my-app/src/index.js
+++ react_task_manager/my-app/src/index.js
@@ -4,27 +4,100 @@ import * as serviceWorker from './serviceWorker';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import {getProjectList, buildProjectInfo, removeProject} from './localStorageProjects.js'
 
+getProjectList()
 
 class ProjectList extends React.Component {
-  render() {
+  constructor(props){
+    super(props);
+    this.state = ({
+          data: buildProjectInfo(),
+          selectedProject: "michael"})
+  }
+  
+  deleteProjectHandler = (index, projectID) =>{
+    removeProject(projectID)
+    console.log(projectID)
+    const projects = [...this.state.data]
+    projects.splice(index, 1)
+    this.setState({data:projects});
+  }
+
+  addProjectHandler = () => {
+    console.log('adding joe')
+    window.localStorage.setItem('joe','{"title":"joe","id":"j"}')
+    const projects = [...this.state.data]
+    projects.push({"title":"joe","id":"j"})
+    this.setState({data:projects})
+  }
+
+  updateSlectedProject = (projectID) =>{
+    this.setState({selectedProject:projectID})
+  }
+
+  render() { 
     return (
       <div className="projectListFrame">
-        <p>Ooops lolol</p>
+        {this.state.data.map((project, index) => (
+          <ProjectListItem 
+            project = {project.title}
+            description = {project.description}
+            complete = {project.complete}
+            tasks = {project.tasks}
+            key = {project.id}
+            selectedProject = {this.state.selectedProject}
+            click = {() => this.deleteProjectHandler(index, project.title)}
+            selectProject = {() => this.updateSlectedProject(project.title)}
+            />
+        ))}
+        <CreateProject 
+          click = {this.addProjectHandler}
+          />
       </div>
     );
   }
 }
 
+class CreateProject extends React.Component{
+  constructor(props){
+    super(props)
+    this.state = null
+  }
+  render(){
+    return(
+      <div className='add-project' onClick = {this.props.click}>Add Project</div>
+    ) 
+  }
+}
+
+class ProjectListItem extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = null
+  }
+
+  render(){
+    console.log(this.props.selectedProject)
+    console.log(this.props.project)
+    return (
+      <div
+      className={this.props.selectedProject === this.props.project ? 'project-item active-project': 'project-item'} 
+      onClick = {this.props.selectProject}>{this.props.project}</div>
+    )
+  }
+}
+
+
 class ProjectDetail extends React.Component {
   render(){
     return (
-      <div className="projectDetailFrame">
-            <p>Hello world!</p>
-          </div>
+      <div className="projectDetailFrame">    
+      </div>
     );
   }
 }
+
 
 class App extends React.Component{
   render(){
@@ -42,10 +115,15 @@ class App extends React.Component{
   }
 }
 
+
+
+
 ReactDOM.render(
     <App />,
     document.getElementById('root')
   );
 
 serviceWorker.unregister();
+
+
 
---
+++ react_task_manager/my-app/src/localStorageProjects.js
@@ -0,0 +1,39 @@
+function getLocalStorage(){
+    console.log(window.localStorage)
+    return window.localStorage
+}
+
+function getCurrentProject(projectTitle){
+    let currentProject = window.localStorage.getItem(projectTitle)
+    currentProject = JSON.parse(currentProject)
+    return currentProject
+}
+
+function saveCurrentProject(project){
+    let projectTitle = project.title
+    let projectDetail = JSON.stringify(project)
+    window.localStorage.setItem(projectTitle, projectDetail)
+}
+
+function getProjectList(){
+    let localStorageEntries = Object.entries(window.localStorage)
+    let projectTitles = localStorageEntries.map(entry => entry[0])
+    return projectTitles
+}
+
+function buildProjectInfo(){
+    let projectList = getProjectList()
+    let projectInfo = []
+    for (let i = 0; i < projectList.length; i++){
+        projectInfo.push(getCurrentProject(projectList[i]))
+    }
+    return projectInfo
+}
+
+function removeProject(project){
+    window.localStorage.removeItem(project)
+}
+
+
+
+export {getLocalStorage, getCurrentProject, getProjectList, buildProjectInfo, removeProject}
