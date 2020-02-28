bf83a25b376b29d9c087868c5dbd0f3e06ceb1ac
Robert <rjbarrett04@gmail.com>
2020-02-28 02:54:10 -0800

    Changed to fetch HTTPS not HTTP.

--- weather-app/weather.js
+++ weather-app/weather.js
@@ -12,12 +12,12 @@ function formatTime(hours, minutes){
 async function getCityWeather(cityName){
     let long 
     let lat
-    const weather = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=imperial&APPID=fd44eb02e519e736eeb68874e1c597c9`)
+    const weather = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=imperial&APPID=fd44eb02e519e736eeb68874e1c597c9`)
     const data = await weather.json()
     
     long = data.coord.lon
     lat = data.coord.lat
-    const time = await fetch(`http://api.timezonedb.com/v2.1/get-time-zone?key=BX35L7KUUB8G&format=json&by=position&lat=${lat}&lng=${long}`)
+    const time = await fetch(`https://api.timezonedb.com/v2.1/get-time-zone?key=BX35L7KUUB8G&format=json&by=position&lat=${lat}&lng=${long}`)
     const timeData = await time.json();
     const timeFormatted = new Date(timeData.formatted) 
     const timeDisplay = formatTime(timeFormatted.getHours(),timeFormatted.getMinutes())
