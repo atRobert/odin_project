7f9d7d380aefc9e4898783f10ee73c01b6bb9149
Robert <rjbarrett04@gmail.com>
2020-03-08 01:33:21 -0800

    Play button no longer blocks bottom blocks.

--- battleship/src/index.js
+++ battleship/src/index.js
@@ -5,6 +5,7 @@ let player
 let computer
 let playButton = document.getElementById('play')
 playButton.addEventListener('click', function(e){
+    document.getElementById('text').style.visibility='hidden'
     this.style.visibility='hidden'
     this.style.opacity='0'
     player=Gameboard()
@@ -16,6 +17,7 @@ playButton.addEventListener('click', function(e){
 
 let resetButton = document.getElementById('reset')
 resetButton.addEventListener('click', function(e){
+    document.getElementById('text').style.visibility='visible'
     document.getElementById('door-right').style.width = '50vw'
     document.getElementById('door-left').style.width = '50vw'
     
@@ -26,7 +28,6 @@ resetButton.addEventListener('click', function(e){
         document.getElementById('player-display').innerHTML = ''
         document.getElementById('computer-display').innerHTML = ''
         document.getElementById('winner-display').textContent =''
-        
         player=Gameboard()
         computer=Gameboard()
         },1200)
